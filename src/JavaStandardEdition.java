4a588f632802b6eb5a235a9cd54182f1712ae23c
JazzoLopez <Jazzielrodriguezlopez@gmail.com>
2025-08-01 00:34:34 -0600

    chore: rename file

--- src/JavaStandardEdition.java
+++ src/JavaStandardEdition.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaEstandardEdition {
+public class JavaStandardEdition {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int year = 2004;
